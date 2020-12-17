badc44bdcb2f04cbae2bbc79b7a9aa08d6067eb7
Sankha Subhra Mondal <sankhasubhramondal18@gmail.com>
2020-12-17 21:28:22 +0530

    Add files via upload

---
+++ static/js/app.js
@@ -0,0 +1,9 @@
+let buttonRight = document.getElementById('slideRight');
+let buttonLeft = document.getElementById('slideLeft');
+
+buttonRight.onclick = function () {
+  document.getElementsByClassName(('cards').scroll += 20;
+};
+buttonLeft.onclick = function () {
+  document.getElementByClassName('cards').scroll -= 20;
+};
